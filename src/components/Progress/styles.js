e250ea9bdf470529a058688415576906c68b38ff
DragonYang <dragonyang0917@gmail.com>
2018-08-31 09:29:54 +0800

    添加进度条组建

---
+++ src/components/Progress/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import { scaleSize } from '../../utils'
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+    height: scaleSize(8),
+    backgroundColor: 'black',
+  },
+})
