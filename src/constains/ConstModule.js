dccefd72bca3cdae244fa520fdd927b4347f9df6
DragonYang <dragonyang0917@gmail.com>
2018-10-22 16:24:52 +0800

    修改首页module数据

--- src/constains/ConstModule.js
+++ src/constains/ConstModule.js
@@ -1,55 +1,55 @@
-const Module = {
-  data: [
-    {
-      key: '地图制图',
-      title: '地图制图',
-      baseimage: require('../assets/home/icon_lefttop_free.png'),
-      moduleimage: require('../assets/home/icon_cartography.png'),
+import NavigationService from '../containers/NavigationService'
+
+export default [
+  {
+    key: '地图制图',
+    title: '地图制图',
+    baseImage: require('../assets/home/icon_lefttop_free.png'),
+    moduleImage: require('../assets/home/icon_cartography.png'),
+  },
+  {
+    key: '三维场景',
+    title: '三维场景',
+    baseImage: require('../assets/home/icon_rightbottom_free.png'),
+    moduleImage: require('../assets/home/icon_map3D.png'),
+  },
+  {
+    key: 'AR地图',
+    title: 'AR地图',
+    baseImage: require('../assets/home/icon_lefttop_vip.png'),
+    moduleImage: require('../assets/home/icon_ARmap.png'),
+  },
+  {
+    key: '导航地图',
+    title: '导航地图',
+    baseImage: require('../assets/home/icon_rightbottom_vip.png'),
+    moduleImage: require('../assets/home/icon_navigation.png'),
+  },
+  {
+    key: '专题地图',
+    title: '专题地图',
+    baseImage: require('../assets/home/icon_lefttop_vip.png'),
+    moduleImage: require('../assets/home/icon_thematicmap.png'),
+  },
+  {
+    key: '外业采集',
+    title: '外业采集',
+    baseImage: require('../assets/home/icon_rightbottom_vip.png'),
+    moduleImage: require('../assets/home/icon_collection.png'),
+    action: params => {
+      NavigationService.navigate('MapView', params)
     },
-    {
-      key: '三维场景',
-      title: '三维场景',
-      baseimage: require('../assets/home/icon_rightbottom_free.png'),
-      moduleimage: require('../assets/home/icon_map3D.png'),
-    },
-    {
-      key: 'AR地图',
-      title: 'AR地图',
-      baseimage: require('../assets/home/icon_lefttop_vip.png'),
-      moduleimage: require('../assets/home/icon_ARmap.png'),
-    },
-    {
-      key: '导航地图',
-      title: '导航地图',
-      baseimage: require('../assets/home/icon_rightbottom_vip.png'),
-      moduleimage: require('../assets/home/icon_navigation.png'),
-    },
-    {
-      key: '专题地图',
-      title: '专题地图',
-      baseimage: require('../assets/home/icon_lefttop_vip.png'),
-      moduleimage: require('../assets/home/icon_thematicmap.png'),
-    },
-    {
-      key: '外业采集',
-      title: '外业采集',
-      baseimage: require('../assets/home/icon_rightbottom_vip.png'),
-      moduleimage: require('../assets/home/icon_collection.png'),
-    },
-    {
-      key: '应急标绘',
-      title: '应急标绘',
-      baseimage: require('../assets/home/icon_lefttop_vip.png'),
-      moduleimage: require('../assets/home/icon_plot.png'),
-    },
-    {
-      key: '数据分析',
-      title: '数据分析',
-      baseimage: require('../assets/home/icon_rightbottom_vip.png'),
-      moduleimage: require('../assets/home/icon_mapanalysis.png'),
-    },
-  ],
-}
-export default {
-  Module,
-}
+  },
+  {
+    key: '应急标绘',
+    title: '应急标绘',
+    baseImage: require('../assets/home/icon_lefttop_vip.png'),
+    moduleImage: require('../assets/home/icon_plot.png'),
+  },
+  {
+    key: '数据分析',
+    title: '数据分析',
+    baseImage: require('../assets/home/icon_rightbottom_vip.png'),
+    moduleImage: require('../assets/home/icon_mapanalysis.png'),
+  },
+]
