99963c3b907281e66f9b46941a96505f83061df8
DragonYang <dragonyang0917@gmail.com>
2018-12-05 08:57:43 +0800

    修复三维手势监听

--- src/containers/workspaceFileList/WorkspaceFlieList.js
+++ src/containers/workspaceFileList/WorkspaceFlieList.js
@@ -37,7 +37,7 @@ export default class WorkSpaceFileList extends Component {
     this.cb = params.cb
     this.state = {
       data: [],
-      backPath: ConstPath.AppPath + '/data/local',
+      backPath: ConstPath.AppPath,
       showData: false,
     }
   }
@@ -156,19 +156,10 @@ export default class WorkSpaceFileList extends Component {
           let result = await SScene.openWorkspace(data)
           let mapList = await SScene.getMapList()
           result && (await SScene.openMap(mapList[0].name))
-          NavigationService.goBack() && this.container.setLoading(false)
-        } catch (error) {
-          Toast.show('打开失败')
-          this.container.setLoading(false)
-        }
-        break
-      default:
-        try {
-          this.container && this.container.setLoading(true, '正在打开地图')
-          let data = { server: path }
-          let result = await SScene.openWorkspace(data)
-          let mapList = await SScene.getMapList()
-          result && (await SScene.openMap(mapList[0].name))
+          SScene.setListener().then(() => {
+            SScene.getAttribute()
+            SScene.setCircleFly()
+          })
           NavigationService.goBack() && this.container.setLoading(false)
         } catch (error) {
           Toast.show('打开失败')
