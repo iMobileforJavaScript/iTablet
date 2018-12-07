30d2500f6d4a6a3fcaa34580eb9658577c5f5a89
xiezhiyan <xiezhiyanfighting@163.com>
2018-12-07 08:52:02 +0800

    local
    local

--- ios/iTablet/AppDelegate.m
+++ ios/iTablet/AppDelegate.m
@@ -26,7 +26,7 @@ static NSString* g_sampleCodeName = @"#";;
   NSURL *jsCodeLocation;
   
 #if DEBUG
-  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.218.111"];
+  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.218.105"];
 #endif
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
