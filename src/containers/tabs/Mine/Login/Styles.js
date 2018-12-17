b2e9c65b99f91066b403d07a848e5494bb7f1cc2
jiushuaizhao <jiushuaizhao@jiushuaizhaodeMac-mini.local>
2018-12-17 19:35:47 +0800

    3Dmerge

--- src/containers/tabs/Mine/Login/Styles.js
+++ src/containers/tabs/Mine/Login/Styles.js
@@ -1,8 +1,10 @@
 import { Dimensions, StyleSheet } from 'react-native'
 import { color } from '../../../../styles'
-const screenWidth = Dimensions.get('window').width
-const screenHeight = Dimensions.get('window').height
-const itemWidth = Dimensions.get('window').width * 0.8
+let screenWidth = Dimensions.get('window').width
+let screenHeight = Dimensions.get('window').height
+let width = screenWidth < screenHeight ? screenWidth : screenHeight
+let height = screenWidth < screenHeight ? screenWidth : screenHeight
+const itemWidth = width * 0.8
 const itemHeight = 40
 const fontSize = 16
 const titleOnFocusBackgroundColor = color.blackBg
@@ -10,14 +12,16 @@ const titleOnBlurBackgroundColor = color.border
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    // flexDirection: 'column',
+    // justifyContent:"space-around",
+    alignItems: 'center',
     backgroundColor: color.border,
   },
   keyboardAvoidingStyle: {
     padding: 9,
     alignItems: 'center',
-    width: screenWidth,
-    height: screenHeight,
+    width: width,
+    height: height,
   },
   titleStyle: {
     marginTop: 20,
