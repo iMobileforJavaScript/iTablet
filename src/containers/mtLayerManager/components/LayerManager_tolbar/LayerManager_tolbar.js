0cca540db72050b332c84a7984ec3ab3bea4a9e6
jiakai <873487220@qq.com>
2019-01-29 17:01:13 +0800

    '分隔符、切换底图关闭弹窗'

--- src/containers/mtLayerManager/components/LayerManager_tolbar/LayerManager_tolbar.js
+++ src/containers/mtLayerManager/components/LayerManager_tolbar/LayerManager_tolbar.js
@@ -207,6 +207,7 @@ export default class LayerManager_tolbar extends React.Component {
       (async function() {
         await section.action()
         this.props.getLayers()
+        this.setVisible(false)
       }.bind(this)())
     }
     if (section.title === '移除') {
