d74d2a28f562162df03c8b27356d37910c5e72c3
DragonYang <dragonyang0917@gmail.com>
2019-03-08 15:29:41 +0800

    添加浅色主题回撤/还原/恢复 图片

--- src/assets/publicTheme/common/index.js
+++ src/assets/publicTheme/common/index.js
@@ -1,5 +1,9 @@
 const icon_search_a0 = require('./icon_search_a0.png')
+const icon_search = require('../../publicTheme/common/icon_search.png')
+const icon_undo = require('../../publicTheme/common/icon_undo.png')
 
 export default {
+  icon_undo,
+  icon_search,
   icon_search_a0,
 }
