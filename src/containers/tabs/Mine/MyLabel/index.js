bd1e3b389a44f1b8274e9337e106eee6b112ced8
jiushuaizhao <jiushuaizhao@jiushuaizhaodeMac-mini.local>
2019-03-15 09:51:02 +0800

    修改mylabel

--- src/containers/tabs/Mine/MyLabel/index.js
+++
@@ -1,9 +0,0 @@
-import MyLabel from './MyLabel'
-import { connect } from 'react-redux'
-const mapStateToProps = state => ({
-  user: state.user.toJS(),
-})
-export default connect(
-  mapStateToProps,
-  {},
-)(MyLabel)
