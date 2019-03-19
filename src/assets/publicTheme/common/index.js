77f75b38ade34de7dfead4bc47eac09eb64c0683
DragonYang <dragonyang0917@gmail.com>
2019-03-19 08:57:51 +0800

    添加图片

--- src/assets/publicTheme/common/index.js
+++ src/assets/publicTheme/common/index.js
@@ -1,9 +1,15 @@
 const icon_search_a0 = require('./icon_search_a0.png')
 const icon_search = require('../../publicTheme/common/icon_search.png')
 const icon_undo = require('../../publicTheme/common/icon_undo.png')
+const icon_check = require('../../publicTheme/common/icon_check.png')
+const icon_uncheck = require('../../publicTheme/common/icon_uncheck.png')
+const icon_check_disable = require('../../publicTheme/common/icon_check_disable.png')
 
 export default {
   icon_undo,
   icon_search,
   icon_search_a0,
+  icon_check,
+  icon_uncheck,
+  icon_check_disable,
 }
---
+++ src/assets/publicTheme/mapTools/index.js
@@ -0,0 +1,5 @@
+const tools_rectangle_cut = require('./tools_rectangle_cut.png')
+
+export default {
+  tools_rectangle_cut,
+}
