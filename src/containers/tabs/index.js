d9109ca2cfb65e370f986d6ad26834b85f462bcc
xiezhiyan <xiezhiyanfighting@163.com>
2019-03-19 10:41:19 +0800

    处理登陆引用错误
    处理登陆引用错误

--- src/containers/tabs/index.js
+++ src/containers/tabs/index.js
@@ -20,6 +20,7 @@ import Mine, {
   MyBaseMap,
 } from './Mine'
 import Find from './Find'
+// eslint-disable-next-line
 import Friend, { Chat, AddFriend, InformMessage } from './Friend'
 
 const Tabs = TabNavigator(
@@ -66,30 +67,30 @@ const Tabs = TabNavigator(
         }
       },
     },
-    Friend: {
-      screen: Friend,
-      navigationOptions: () => {
-        return {
-          tabBarLabel: data => {
-            return (
-              <View style={styles.labelView}>
-                <Image
-                  resizeMode="contain"
-                  source={
-                    data.focused
-                      ? getThemeAssets().tabBar.tab_friend_selected
-                      : getThemeAssets().tabBar.tab_friend
-                  }
-                  style={styles.icon}
-                />
-                <Text style={styles.tabText}>好友</Text>
-              </View>
-            )
-          },
-          header: null,
-        }
-      },
-    },
+    // Friend: {
+    //   screen: Friend,
+    //   navigationOptions: () => {
+    //     return {
+    //       tabBarLabel: data => {
+    //         return (
+    //           <View style={styles.labelView}>
+    //             <Image
+    //               resizeMode="contain"
+    //               source={
+    //                 data.focused
+    //                   ? getThemeAssets().tabBar.tab_friend_selected
+    //                   : getThemeAssets().tabBar.tab_friend
+    //               }
+    //               style={styles.icon}
+    //             />
+    //             <Text style={styles.tabText}>好友</Text>
+    //           </View>
+    //         )
+    //       },
+    //       header: null,
+    //     }
+    //   },
+    // },
     Find: {
       screen: Find,
       navigationOptions: () => {
