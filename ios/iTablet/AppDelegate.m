7f67ca6f9ed790f487569c406b2db820b1e787e8
AsortKeven <976484307@qq.com>
2019-04-02 15:36:07 +0800

    合并三维 设置图层可选和设置图层不可选

--- ios/iTablet/AppDelegate.m
+++ ios/iTablet/AppDelegate.m
@@ -33,7 +33,7 @@ static NSString* g_sampleCodeName = @"#";;
   NSURL *jsCodeLocation;
   
 #if DEBUG
-  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"localhost"];
+  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.137.193"];
 #endif
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   
