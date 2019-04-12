8478c9316e8ecfc3585620f5e0787c02c61115a5
xiezhiyan <xiezhiyanfighting@163.com>
2019-04-12 17:42:11 +0800

    Update MsgConstant.js

--- src/containers/tabs/Friend/MsgConstant.js
+++ src/containers/tabs/Friend/MsgConstant.js
@@ -1,14 +1,14 @@
 export default {
-  MSG_IP:  '192.168.0.111',
-  MSG_Port:  5672,
-  MSG_HostName: '/',
-  MSG_UserName: 'androidtest',
-  MSG_Password: 'androidtest',
-  // MSG_IP: '111.202.121.144',
-  // MSG_Port: 5672,
+  // MSG_IP:  '192.168.0.111',
+  // MSG_Port:  5672,
   // MSG_HostName: '/',
-  // MSG_UserName: 'admin',
-  // MSG_Password: 'admin',
+  // MSG_UserName: 'androidtest',
+  // MSG_Password: 'androidtest',
+  MSG_IP: '111.202.121.144',
+  MSG_Port: 5672,
+  MSG_HostName: '/',
+  MSG_UserName: 'admin',
+  MSG_Password: 'admin',
   MSG_SINGLE: 1, //单人消息
   MSG_GROUP: 2, //群组消息
   MSG_FILE: 3, //文件本体
