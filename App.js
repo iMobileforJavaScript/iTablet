699d51ba846dd9ff6faf0cc728c43edcc488f464
jiakai <873487220@qq.com>
2019-04-17 10:31:55 +0800

    '标注新设计更新'

--- App.js
+++ App.js
@@ -129,12 +129,16 @@ class AppRoot extends Component {
     GLOBAL.loginTimer = undefined
     // TODO 动态切换主题，将 GLOBAL.ThemeType 放入Redux中管理
     GLOBAL.ThemeType = ThemeType.LIGHT_THEME
+    GLOBAL.CurrentUserName = this.props.user.currentUser.userName
+    GLOBAL.TaggingDatasetName = ''
     PT.initCustomPrototype()
   }
   UNSAFE_componentWillMount(){
     SOnlineService.init()
   }
   componentDidMount () {
+    GLOBAL.CurrentUserName = this.props.user.currentUser.userName
+
 
     if(GLOBAL.loginTimer !== undefined){
       clearInterval(GLOBAL.loginTimer)
