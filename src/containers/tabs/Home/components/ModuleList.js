d4c56a100f5522c9ef708290b9a1c65dcfa23817
xiezhiyan <xiezhiyanfighting@163.com>
2019-04-23 08:51:51 +0800

    提交遗漏
    提交遗漏

--- src/containers/tabs/Home/components/ModuleList.js
+++ src/containers/tabs/Home/components/ModuleList.js
@@ -388,20 +388,19 @@ export class ModuleList extends Component {
           disabled: false,
           isShowProgressView: false,
         })
-
-        let latestMap
-        if (
-          this.props.latestMap[currentUserName] &&
-          this.props.latestMap[currentUserName][module] &&
-          this.props.latestMap[currentUserName][module].length > 0
-        ) {
-          latestMap = this.props.latestMap[currentUserName][module][0]
-          // if(latestMap.name===mapname){
-          //   latestMap=null
-          // }
-        }
-        item.action && item.action(tmpCurrentUser, latestMap)
       }
+      let latestMap
+      if (
+        this.props.latestMap[currentUserName] &&
+        this.props.latestMap[currentUserName][module] &&
+        this.props.latestMap[currentUserName][module].length > 0
+      ) {
+        latestMap = this.props.latestMap[currentUserName][module][0]
+        // if(latestMap.name===mapname){
+        //   latestMap=null
+        // }
+      }
+      item.action && item.action(tmpCurrentUser, latestMap)
     } catch (e) {
       this.moduleItems[index].setNewState({
         disabled: false,
