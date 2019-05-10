65d8aac9387e0a40db7d533a0aef0e6a1bbf83f2
AsortKeven <976484307@qq.com>
2019-05-10 16:04:36 +0800

    地图设置菜单，目前仅开了专题制图，其他模块暂时屏蔽 安卓缺少对应接口 暂时屏蔽

---
+++ src/containers/mapSetting/secondMapSettings/secondMapSettings.js
@@ -0,0 +1,236 @@
+/**
+ * Copyright © SuperMap. All rights reserved.
+ * Author: Asort
+ * https://github.com/AsortKeven
+ */
+import React, { Component } from 'react'
+import { Container } from '../../../components'
+// import constants from '../../workspace/constants'
+// // import NavigationService from '../NavigationService'
+// import { MapToolbar } from '../../workspace/components'
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Image,
+  Switch,
+  Platform,
+} from 'react-native'
+import {
+  basicSettings,
+  rangeSettings,
+  coordinateSystemSettings,
+  advancedSettings,
+} from '../settingData'
+import NavigationService from '../../NavigationService'
+// import styles from './styles'
+import { SMap } from 'imobile_for_reactnative'
+// import { getLanguage } from '../../../language/index'
+// import Toast from "../../../utils/Toast"
+import { scaleSize } from '../../../utils'
+import color from '../../../styles/color'
+import styles from './styles'
+
+export default class secondMapSettings extends Component {
+  props: {
+    language: string,
+    navigation: Object,
+    currentMap: Object,
+    setMapSetting: () => {},
+    mapSetting: any,
+    device: Object,
+    mapLegend: boolean,
+    setMapLegend: () => {},
+    title: string,
+  }
+  constructor(props) {
+    super(props)
+    let { params } = this.props.navigation.state
+    this.state = {
+      data: '',
+      title: params.title,
+    }
+  }
+  UNSAFE_componentWillMount() {
+    this.getData()
+  }
+  getData = async () => {
+    let data
+    //地图旋转角度
+    switch (this.state.title) {
+      case '基本设置':
+        data = await this.getBasicData()
+        break
+      case '范围设置':
+        data = await this.getRangeData()
+        break
+      case '坐标系设置':
+        data = await this.getCoordinateSystemData()
+        break
+      case '高级设置':
+        data = await this.getAdvanceData()
+        break
+      case '图例设置':
+    }
+    this.setState({
+      data,
+    })
+  }
+
+  getBasicData = async () => {
+    let data
+    let angle
+    data = await basicSettings()
+
+    if (Platform.OS === 'ios') {
+      data[0].value = await SMap.getMapName()
+      data[1].value = await SMap.getScaleViewEnable()
+      angle = await SMap.getMapAngle()
+      data[2].value = angle.toString().replace('.0', '')
+      data[3].value = await SMap.getMapColorMode()
+      //todo 4背景颜色 6固定符号角度 7固定文本角度 8固定文本方向 待设置
+      data[5].value = await SMap.isAntialias()
+      data[9].value = await SMap.isOverlapDisplayed()
+    }
+
+    return data
+  }
+
+  getRangeData = async () => {
+    let data = await rangeSettings()
+    return data
+  }
+
+  getCoordinateSystemData = async () => {
+    let data = await coordinateSystemSettings()
+    return data
+  }
+
+  getAdvanceData = async () => {
+    let data = await advancedSettings()
+    return data
+  }
+
+  backAction = () => {
+    NavigationService.goBack()
+  }
+
+  _onValueChange = async ({ value, item, index }) => {
+    if (Platform.OS === 'ios') {
+      let data = this.state.data.concat()
+      switch (item.title) {
+        case '显示比例尺':
+          await SMap.setScaleViewEnable(value)
+          break
+        case '地图反走样':
+          //已有接口内写的是int类型的参数 所以转成数字
+          await SMap.setAntialias(+value)
+          break
+        case '显示压盖对象':
+          await SMap.setOverlapDisplayed(value)
+          break
+      }
+      data[index].value = value
+      this.setState({
+        data,
+      })
+    }
+  }
+
+  renderSwitchItem = (item, index) => {
+    return (
+      <View>
+        <View style={{ ...styles.row, paddingRight: scaleSize(10) }}>
+          <Text style={styles.itemName}>{item.title}</Text>
+          <Switch
+            style={styles.switch}
+            trackColor={{ false: color.bgG, true: color.switch }}
+            thumbColor={item.value ? color.bgW : color.bgW}
+            ios_backgroundColor={item.value ? color.switch : color.bgG}
+            value={item.value}
+            onValueChange={value => {
+              this._onValueChange({ value, item, index })
+            }}
+          />
+        </View>
+        {this.renderLine()}
+      </View>
+    )
+  }
+
+  renderLine = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          height: 1,
+          marginHorizontal: 10,
+          backgroundColor: color.separateColorGray,
+        }}
+      />
+    )
+  }
+
+  renderArrowItem = item => {
+    let rightImagePath = require('../../../assets/Mine/mine_my_arrow.png')
+    return (
+      <View>
+        <TouchableOpacity>
+          <View onPress={() => this.onItemPress(item.title)} style={styles.row}>
+            <Text style={styles.itemName}>{item.title}</Text>
+            {item.value !== undefined && (
+              <Text style={styles.rightText}>{item.value}</Text>
+            )}
+            <Image
+              style={styles.itemArrow}
+              resizeMode={'contain'}
+              source={rightImagePath}
+            />
+          </View>
+        </TouchableOpacity>
+        {this.renderLine()}
+      </View>
+    )
+  }
+
+  renderTextItem = item => {
+    return (
+      <View>
+        <View style={styles.row}>
+          <Text style={styles.itemName}>{item.title}</Text>
+        </View>
+        {this.renderLine()}
+      </View>
+    )
+  }
+
+  renderItem = ({ item, index }) => {
+    switch (item.iconType) {
+      case 'switch':
+        return this.renderSwitchItem(item, index)
+      case 'arrow':
+        return this.renderArrowItem(item)
+      case 'text':
+        return this.renderTextItem(item)
+    }
+  }
+
+  render() {
+    return (
+      <Container
+        headerProps={{
+          title: this.state.title,
+          backAction: this.backAction,
+        }}
+      >
+        <FlatList
+          renderItem={this.renderItem}
+          data={this.state.data}
+          keyExtractor={(item, index) => item + index}
+          numColumns={1}
+        />
+      </Container>
+    )
+  }
+}
--- src/containers/mapSetting/settingData.js
+++ src/containers/mapSetting/settingData.js
@@ -121,4 +121,185 @@ function getMapSettings() {
   ]
   return data
 }
-export { getMapSettings }
+// 地图设置 新菜单栏
+//todo 中英文
+const getThematicMapSettings = () => [
+  {
+    title: '基本设置',
+  },
+  {
+    title: '范围设置',
+  },
+  {
+    title: '坐标系设置',
+  },
+  {
+    title: '高级设置',
+  },
+  {
+    title: '图例设置',
+  },
+]
+/*
+ * 二级 三级地图菜单 设置
+ * */
+//基本设置
+const basicSettings = () => [
+  {
+    title: '地图名称',
+    value: '',
+    iconType: 'arrow',
+  },
+  {
+    title: '显示比例尺',
+    value: false,
+    iconType: 'switch',
+  },
+  // {
+  //   title:'显示指北针',
+  //   value:false,
+  //   iconType:'switch',
+  // },
+  {
+    title: '旋转角度',
+    value: '0°',
+    iconType: 'arrow',
+  },
+  {
+    title: '颜色模式',
+    value: '',
+    iconType: 'arrow',
+  },
+  {
+    title: '背景颜色',
+    iconType: 'text',
+  },
+  {
+    title: '地图反走样',
+    value: false,
+    iconType: 'switch',
+  },
+  // {
+  //   title:'线性反走样',
+  //   value:true,
+  //   iconType:'switch',
+  // },
+  {
+    title: '固定符号角度',
+    value: true,
+    iconType: 'switch',
+  },
+  {
+    title: '固定文本角度',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '固定文本方向',
+    value: true,
+    iconType: 'switch',
+  },
+  {
+    title: '显示压盖对象',
+    value: true,
+    iconType: 'switch',
+  },
+]
+//范围设置
+const rangeSettings = () => [
+  {
+    title: '中心点',
+    value: '120.224544/23.354585',
+    iconType: 'arrow',
+  },
+  {
+    title: '比例尺',
+    value: '1:2013,751.254445',
+    iconType: 'arrow',
+  },
+  {
+    title: '固定比例尺级别',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '当前窗口四至范围',
+    iconType: 'text',
+  },
+]
+//坐标系设置
+const coordinateSystemSettings = () => [
+  {
+    title: '坐标轴',
+    value: 'GCS_WGS 1984',
+    iconType: 'arrow',
+  },
+  {
+    title: '动态投影',
+    value: true,
+    iconType: 'switch',
+  },
+  {
+    title: '转换参数',
+    iconType: 'arrow',
+  },
+]
+//高级设置
+const advancedSettings = () => [
+  {
+    title: '流动显示',
+    value: true,
+    iconType: 'switch',
+  },
+  {
+    title: '显示负值数据',
+    value: true,
+    iconType: 'switch',
+  },
+  {
+    title: '自动避让',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '随图缩放',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '显示牵引线',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '全据统计值',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '统计图注记',
+    value: '百分比',
+    iconType: 'arrow',
+  },
+  {
+    title: '显示坐标轴',
+    value: false,
+    iconType: 'switch',
+  },
+  {
+    title: '柱状图风格',
+    iconType: 'arrow',
+  },
+  {
+    title: '玫瑰图、饼图风格',
+    iconType: 'arrow',
+  },
+]
+export {
+  getMapSettings,
+  getThematicMapSettings,
+  basicSettings,
+  rangeSettings,
+  coordinateSystemSettings,
+  advancedSettings,
+}
