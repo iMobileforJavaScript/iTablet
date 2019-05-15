6c0421e6f076115665da264ab02972943233b9e2
xiezhiyan <xiezhiyanfighting@163.com>
2019-05-15 17:05:34 +0800

    zxt 好友群组管理页面

--- src/assets/lightTheme/friend/index.js
+++ src/assets/lightTheme/friend/index.js
@@ -1,7 +1,23 @@
 const friend_add = require('./app_friend_add.png')
 const friend_search = require('./app_friend_search.png')
+const friend_blackList = require('./icon_contact_block_light.png')
+const friend_edit = require('./icon_contact_editcon_light.png')
+const friend_map = require('./icon_contact_map_light.png')
+const friend_message = require('./icon_contact_message_light.png')
+const friend_mobileCon = require('./icon_contact_mobilecon_light.png')
+const friend_notice = require('./icon_contact_notice_light.png')
+const friend_scan = require('./icon_contact_scan_light.png')
+const friend_shareCon = require('./icon_contact_sharecon_light.png')
 
 export default {
   friend_add,
   friend_search,
+  friend_blackList,
+  friend_edit,
+  friend_map,
+  friend_message,
+  friend_mobileCon,
+  friend_notice,
+  friend_scan,
+  friend_shareCon,
 }
