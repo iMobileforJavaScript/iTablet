b0770f22c3f6aa0a9f6863079429a426da115e43
xiezhiyan <xiezhiyanfighting@163.com>
2019-05-31 16:29:53 +0800

    分离三维功能
    分离三维功能

--- ios/iTablet/AppDelegate.m
+++ ios/iTablet/AppDelegate.m
@@ -34,7 +34,7 @@ static NSString* g_sampleCodeName = @"#";;
   NSURL *jsCodeLocation;
   
 #if DEBUG
-  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.0.102"];
+  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.43.228"];
 #endif
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   
