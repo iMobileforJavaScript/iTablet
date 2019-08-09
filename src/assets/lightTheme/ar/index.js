0fe80fd960d537c7d039c9b69db85a9cf5fa0e71
tron9527 <278042465@qq.com>
2019-08-09 17:02:39 +0800

    新增高精量算采集界面MeasureView

--- src/assets/lightTheme/ar/index.js
+++ src/assets/lightTheme/ar/index.js
@@ -1,6 +1,12 @@
 const icon_ar = require('./icon_ar.png')
 const icon_ai_assistant = require('./icon_ai_assistant.png')
+const icon_ai_measure_cancel = require('./cancel.png')
+const icon_ai_measure_flag = require('./flag.png')
+const icon_ai_measure_cross = require('./cross.png')
 export default {
   icon_ar,
   icon_ai_assistant,
+  icon_ai_measure_cancel,
+  icon_ai_measure_flag,
+  icon_ai_measure_cross,
 }
