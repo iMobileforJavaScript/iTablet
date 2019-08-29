c0b9dabe54fb992f775d639430ed9dd185dbd759
DragonYang <dragonyang0917@gmail.com>
2019-08-29 11:10:00 +0800

    新增智能配图图标

--- src/assets/lightTheme/mapTools/index.js
+++ src/assets/lightTheme/mapTools/index.js
@@ -11,6 +11,8 @@ const icon_point_line_disable = require('./icon_point_line_disable.png')
 const icon_region_disable = require('./icon_region_disabled.png')
 const icon_free_region_disable = require('./icon_free_region_disable.png')
 
+const rightbar_tool_style = require('./rightbar_tool_style_light.png')
+
 export default {
   icon_point_disable,
   icon_free_line_disable,
@@ -18,4 +20,5 @@ export default {
   icon_point_line_disable,
   icon_region_disable,
   icon_free_region_disable,
+  rightbar_tool_style,
 }
