3a758b48694bf1126ef9268bb943bbb5b2c2f8c3
xiezhiyan <xiezhiyanfighting@163.com>
2019-09-26 17:58:55 +0800

    zxt 图片下载

--- src/containers/tabs/Friend/Component/ImageViewer.js
+++ src/containers/tabs/Friend/Component/ImageViewer.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
-import { Modal } from 'react-native'
+import {
+  Modal,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from 'react-native'
 import ImageViewer from 'react-native-image-zoom-viewer'
 
 export default class ChatImageViewer extends React.Component {
   props: {
     imgUri: String,
+    receivePicture: () => {},
   }
 
   constructor(props) {
@@ -13,6 +20,7 @@ export default class ChatImageViewer extends React.Component {
       imageUrls: [{ url: props.imgUri }],
       visible: false,
     }
+    this.screenWidth = Dimensions.get('window').width
   }
 
   setVisible = visible => {
@@ -27,11 +35,46 @@ export default class ChatImageViewer extends React.Component {
     })
   }
 
-  render() {
+  setPicMsg = message => {
+    this.message = message
+  }
+
+  receivePicture = () => {
+    if (!this.message.originMsg.message.message.filePath) {
+      this.props.receivePicture && this.props.receivePicture(this.message)
+    }
+  }
+
+  renderImageFooter = () => {
     return (
-      <Modal visible={this.state.visible} transparent={true}>
+      <TouchableOpacity
+        onPress={this.receivePicture}
+        style={styles.bottomMenuStyle}
+      >
+        <Text>123</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  render() {
+    this.screenWidth = Dimensions.get('window').width
+    return (
+      <Modal
+        animationType={'fade'}
+        onBackButtonPress={() => {
+          this.setVisible(false)
+        }}
+        visible={this.state.visible}
+        transparent={true}
+      >
         <ImageViewer
           imageUrls={this.state.imageUrls}
+          // renderFooter={this.renderImageFooter}
+          footerContainerStyle={[
+            styles.footerContainerStyle,
+            { width: this.screenWidth },
+          ]}
+          saveToLocalByLongPress={false}
           onClick={() => {
             this.setVisible(false)
           }}
@@ -40,3 +83,16 @@ export default class ChatImageViewer extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  bottomMenuStyle: {
+    bottom: 100,
+    alignSelf: 'center',
+    backgroundColor: 'red',
+    width: 200,
+    height: 50,
+  },
+  footerContainerStyle: {
+    alignItems: 'center',
+  },
+})
