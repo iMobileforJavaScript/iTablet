31a8ada9dc7ea0fd98a70d4bd1f1249c0d6d6555
AsortKeven <976484307@qq.com>
2019-10-16 09:00:57 +0800

    导航页面弹窗

---
+++ src/assets/publicTheme/navigation/index.js
@@ -0,0 +1,8 @@
+const icon_nav_start = require('./icon_nav_start.png')
+const icon_nav_end = require('./icon_nav_end.png')
+
+export default {
+  //导航起点终点
+  icon_nav_start,
+  icon_nav_end,
+}
---
+++ src/containers/workspace/components/SimpleSelectList/index.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright © SuperMap. All rights reserved.
+ * Author: Asort
+ * https://github.com/AsortKeven
+ */
+
+import SimpleSelectList from './SimpleSelectList'
+export default SimpleSelectList
