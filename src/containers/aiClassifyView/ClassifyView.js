db2526aedbc5d31dc13e09705f579669c84ac347
xiezhiyan <xiezhiyanfighting@163.com>
2019-12-17 15:22:38 +0800

    zym修改实景情况下进行锁屏，再进入app，画面卡死
    zym修改实景情况下进行锁屏，再进入app，画面卡死

--- src/containers/aiClassifyView/ClassifyView.js
+++ src/containers/aiClassifyView/ClassifyView.js
@@ -124,11 +124,11 @@ export default class ClassifyView extends React.Component {
       this.prevAppstate = appState
       this.stateChangeCount = 0
       if (appState === 'active') {
-        // this.clear()
-        // this.startPreview()
+        this.clear()
+        this.startPreview()
       } else if (appState === 'background') {
-        // this.pausePreview()
-        this.back()
+        this.pausePreview()
+        // this.back()
       }
     }
   }
