21067b31936bfdb8de084bda870cfd1c5b17ea6e
AsortKeven <976484307@qq.com>
2019-12-25 17:33:51 +0800

    IOS 属性表 手动滚动到编辑位置

--- src/containers/layerAttribute/components/LayerAttributeTable/Cell.js
+++ src/containers/layerAttribute/components/LayerAttributeTable/Cell.js
@@ -104,8 +104,8 @@ export default class Cell extends Component {
   _onBlur = () => {
     this._onSubmitEditing()
   }
-  _onFocus = () => {
-    this.props.onFocus && this.props.onFocus()
+  _onFocus = evt => {
+    this.props.onFocus && this.props.onFocus(evt)
   }
   changeEnd = () => {
     if (
@@ -121,7 +121,8 @@ export default class Cell extends Component {
     }
   }
 
-  _onPress = () => {
+  _onPress = evt => {
+    this._onFocus(evt)
     if (this.props.index === 0) {
       if (this.props.onPress && typeof this.props.onPress === 'function') {
         this.props.onPress({
@@ -241,7 +242,6 @@ export default class Cell extends Component {
             underlineColorAndroid="transparent"
             onChangeText={this._onChangeText}
             onBlur={this._onBlur}
-            onFocus={this._onFocus}
             onEndEditing={this._onEndEditing}
             onSubmitEditing={this._onSubmitEditing}
             returnKeyType={'done'}
