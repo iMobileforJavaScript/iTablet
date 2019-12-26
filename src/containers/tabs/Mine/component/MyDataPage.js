7ca0ca9bdbb07462dc48687a8f758a1519d54da0
xiezhiyan <xiezhiyanfighting@163.com>
2019-12-26 14:29:19 +0800

    zym修改ios创建布尔型属性字段不选择默认值创建后有默认值缺陷
    zym修改ios创建布尔型属性字段不选择默认值创建后有默认值缺陷

--- src/containers/tabs/Mine/component/MyDataPage.js
+++ src/containers/tabs/Mine/component/MyDataPage.js
@@ -8,6 +8,7 @@ import {
   NativeModules,
   RefreshControl,
   ScrollView,
+  Platform,
 } from 'react-native'
 import { Container, ListSeparator, InputDialog } from '../../../../components'
 import { ConstPath, ConstInfo } from '../../../../constants'
@@ -369,8 +370,13 @@ export default class MyDataPage extends Component {
 
   shareToWechat = async fileName => {
     let result
+    let isInstalled
+    if (Platform.OS === 'ios') {
+      isInstalled = true
+    } else {
+      isInstalled = await appUtilsModule.isWXInstalled()
+    }
     // let isInstalled = await appUtilsModule.isWXInstalled()
-    let isInstalled = true
     if (isInstalled) {
       await this.exportData(fileName)
       let homePath = await FileTools.appendingHomeDirectory()
