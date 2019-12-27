aaa7a6591f44cdcc86a4a69dad2356e0fb3400e6
xiezhiyan <xiezhiyanfighting@163.com>
2019-12-27 15:54:49 +0800

    zym修改框选和点选的对象后查看属性再添加属性字段提示信息有黑块缺陷
    zym修改框选和点选的对象后查看属性再添加属性字段提示信息有黑块缺陷

--- src/containers/layerAttribute/pages/layerSelectionAttribute/LayerAttributeTabs.js
+++ src/containers/layerAttribute/pages/layerSelectionAttribute/LayerAttributeTabs.js
@@ -343,9 +343,7 @@ export default class LayerAttributeTabs extends React.Component {
       let result = await SMap.addAttributeFieldInfo(layerPath, true, fieldInfo)
       if (result) {
         Toast.show(
-          Toast.show(
-            getLanguage(this.props.language).Prompt.ATTRIBUTE_ADD_SUCCESS,
-          ),
+          getLanguage(this.props.language).Prompt.ATTRIBUTE_ADD_SUCCESS,
         )
         this.currentTabRefs[this.state.currentTabIndex].getAttribute(
           {
@@ -359,11 +357,7 @@ export default class LayerAttributeTabs extends React.Component {
           () => {},
         )
       } else {
-        Toast.show(
-          Toast.show(
-            getLanguage(this.props.language).Prompt.ATTRIBUTE_ADD_FAILED,
-          ),
-        )
+        Toast.show(getLanguage(this.props.language).Prompt.ATTRIBUTE_ADD_FAILED)
       }
     }
   }
